f544c7d3b6aa447169a1d149b4f373ff59a77b2c
Minhaj Abdul Rahim <minhajabdulraheem@gmail.com>
2022-08-05 16:52:58 +0000

    Developed HTML Structure

---
+++ src/lib/todoItem.svelte
@@ -0,0 +1,13 @@
+<div>
+    <form action="" method="">
+        <input type="hidden" name="done" value="" />
+        <button aria-label="Mark Done/Not-Done">Done/NotDone</button>
+    </form>
+    <form action="" method="">
+        <input type="text" />
+        <button aria-label="Save Todo">Save</button>
+    </form>
+    <form action="" method="">
+        <button aria-label="Delete todo">Delete </button>
+    </form>
+</div>
